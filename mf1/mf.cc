8600bf3666f14ffa65eabb6ff3c31b852f1b2547
Marrocco-Simone <marrocco.simone00@gmail.com>
2024-04-24 17:04:22 +0200

    mf1

--- mf1/mf.cc
+++ mf1/mf.cc
@@ -1,3 +1,26 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+float median(vector<float> &v)
+{
+  int n = v.size();
+  if (n % 2 == 0)
+  {
+    int k = n / 2;
+    nth_element(v.begin(), v.begin() + k, v.end());
+    nth_element(v.begin(), v.begin() + k - 1, v.end());
+    return (v[k] + v[k - 1]) / 2;
+  }
+  else
+  {
+    int k = (n - 1) / 2;
+    nth_element(v.begin(), v.begin() + k, v.end());
+    return v[k];
+  }
+}
+
 /*
 This is the function you need to implement. Quick reference:
 - input rows: 0 <= y < ny
@@ -7,5 +30,21 @@ This is the function you need to implement. Quick reference:
   max(x-hx, 0) <= a < min(x+hx+1, nx), max(y-hy, 0) <= b < min(y+hy+1, ny)
   in out[x + y*nx].
 */
-void mf(int ny, int nx, int hy, int hx, const float *in, float *out) {
+void mf(int ny, int nx, int hy, int hx, const float *in, float *out)
+{
+  for (int x = 0; x < nx; x++)
+  {
+    for (int y = 0; y < ny; y++)
+    {
+      vector<float> values = {};
+      for (int i = max(0, x - hx); i < min(nx, x + hx + 1); i++)
+      {
+        for (int j = max(0, y - hy); j < min(ny, y + hy + 1); j++)
+        {
+          values.push_back(in[i + nx * j]);
+        }
+      }
+      out[x + nx * y] = median(values);
+    }
+  }
 }
