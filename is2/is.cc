c471aefe666099d0fe7a5ce67d9a4a8b3f4bb341
Marrocco-Simone <marrocco.simone00@gmail.com>
2024-05-05 12:44:23 +0300

    ultra fast is2

--- is2/is.cc
+++ is2/is.cc
@@ -1,14 +1,8 @@
-// #include <limits>
-// TODO delete
-#include <cstdio>
 #include <vector>
 using namespace std;
 
-// constexpr double infty = numeric_limits<double>::infinity();
 constexpr double infty = 1.7e308;
 constexpr int C = 3;
-// TODO delete
-constexpr double epsilon = 1e-10;
 
 struct Result
 {
@@ -28,66 +22,6 @@ int id(int x, int y, int c, int nx)
 double color(int x, int y, int c, int nx, const float *data)
 {
     return data[id(x, y, c, nx)];
-}
-
-double error(int x, int y, int c, int nx, const float *data, double *area)
-{
-    return area[c] - color(x, y, c, nx, data);
-}
-
-bool is_inside(int x, int y, int x0, int x1, int y0, int y1)
-{
-    return x >= x0 && x < x1 && y >= y0 && y < y1;
-}
-
-double squared_error(int x0, int x1, int y0, int y1, int nx, int ny, const float *data, double *outer, double *inner)
-{
-    double sq_err = 0.0;
-    for (int x = 0; x < nx; x++)
-    {
-        for (int y = 0; y < ny; y++)
-        {
-            double *area;
-            if (is_inside(x, y, x0, x1, y0, y1))
-                area = inner;
-            else
-                area = outer;
-            for (int c = 0; c < C; c++)
-            {
-                double err = error(x, y, c, nx, data, area);
-                sq_err += err * err;
-            }
-        }
-    }
-    return sq_err;
-}
-
-void avg_colors(int x0, int x1, int y0, int y1, int nx, int ny, const float *data, double *outer, double *inner)
-{
-    for (int c = 0; c < C; c++)
-        outer[c] = inner[c] = 0;
-
-    int inner_points = (x1 - x0) * (y1 - y0);
-    int outer_points = nx * ny - inner_points;
-    // printf("inner points: %d * %d = %d\n", x1 - x0, y1 - y0, inner_points);
-
-    for (int x = 0; x < nx; x++)
-    {
-        for (int y = 0; y < ny; y++)
-        {
-            bool inside = is_inside(x, y, x0, x1, y0, y1);
-            // printf("(%d,%d) is inside: %d\n", x, y, inside);
-            for (int c = 0; c < C; c++)
-            {
-                // printf("color %d: %f,\t", c, color(x, y, c, nx, data));
-                if (inside)
-                    inner[c] += color(x, y, c, nx, data) / inner_points;
-                else
-                    outer[c] += color(x, y, c, nx, data) / outer_points;
-            }
-            // printf("\n");
-        }
-    }
 }
 
 /** O(n^2)*/
@@ -177,6 +111,7 @@ void calculate_sum_squared_from_zero(int ny, int nx, const float *data, vector<d
             }
 }
 
+/** O(1) */
 int calculate_in_points(int x0, int x1, int y0, int y1)
 {
     return (y1 - y0 + 1) * (x1 - x0 + 1);
@@ -302,6 +237,7 @@ void calculate_avg_out_color(int x0, int x1, int y0, int y1, int nx, int ny, dou
     }
 }
 
+/** O(1) */
 void calculate_out_squared_sum(int nx, int ny, double *in, vector<double> const &sum_squared_from_zero, double *out)
 {
     for (int c = 0; c < C; c++)
@@ -310,16 +246,16 @@ void calculate_out_squared_sum(int nx, int ny, double *in, vector<double> const 
     }
 }
 
-double
-calculate_in_error(double *in, double *in_squared_sum, int in_points)
+/** O(1) */
+double calculate_in_error(double *in, double *in_squared_sum, int in_points)
 {
     double sum0 = in_squared_sum[0] - in_points * in[0] * in[0];
     double sum1 = in_squared_sum[1] - in_points * in[1] * in[1];
     double sum2 = in_squared_sum[2] - in_points * in[2] * in[2];
-    // printf("%f + %f + %f = %f\n", sum0, sum1, sum2, sum0 + sum1 + sum2);
     return sum0 + sum1 + sum2;
 }
 
+/** O(1) */
 double calculate_out_error(double *out, double *out_squared_sum, int in_points, int full_points)
 {
     int out_points = full_points - in_points;
@@ -344,16 +280,6 @@ Result segment(int ny, int nx, const float *data)
     vector<double> sum_squared_from_zero(C * nx * ny, 0.0);
     calculate_sum_squared_from_zero(ny, nx, data, sum_squared_from_zero);
 
-    // TODO delete
-    for (int x = 0; x < nx; x++)
-        for (int y = 0; y < ny; y++)
-            printf("%f %f %f\n", avg_from_zero[id(x, y, 0, nx)], avg_from_zero[id(x, y, 1, nx)], avg_from_zero[id(x, y, 2, nx)]);
-    printf("\n");
-    for (int x = 0; x < nx; x++)
-        for (int y = 0; y < ny; y++)
-            printf("%f %f %f\n", sum_squared_from_zero[id(x, y, 0, nx)], sum_squared_from_zero[id(x, y, 1, nx)], sum_squared_from_zero[id(x, y, 2, nx)]);
-    printf("\n");
-
     for (int xdim = 1; xdim <= nx; xdim++)
     {
         for (int ydim = 1; ydim <= ny; ydim++)
@@ -367,13 +293,16 @@ Result segment(int ny, int nx, const float *data)
                     double outer[C] = {0.0};
                     double inner[C] = {0.0};
 
-                    // printf("x0: %d, x1: %d, y0: %d, y1: %d\n", x0, x1, y0, y1);
-                    avg_colors(x0, x1, y0, y1, nx, ny, data, outer, inner);
-                    double sq_err = squared_error(x0, x1, y0, y1, nx, ny, data, outer, inner);
-                    // printf("inner: %f,%f,%f\n", inner[0], inner[1], inner[2]);
-                    // printf("outer: %f,%f,%f\n", outer[0], outer[1], outer[2]);
-                    // printf("error = %f\n", sq_err);
-                    // printf("\n");
+                    calculate_avg_in_color(x0, x1 - 1, y0, y1 - 1, nx, avg_from_zero, inner);
+                    calculate_avg_out_color(x0, x1 - 1, y0, y1 - 1, nx, ny, inner, total_avg, outer);
+
+                    double outer_squared_sums[C] = {0.0};
+                    double inner_squared_sums[C] = {0.0};
+                    calculate_in_squared_sum(x0, x1 - 1, y0, y1 - 1, nx, sum_squared_from_zero, inner_squared_sums);
+                    calculate_out_squared_sum(nx, ny, inner_squared_sums, sum_squared_from_zero, outer_squared_sums);
+                    int in_points = calculate_in_points(x0, x1 - 1, y0, y1 - 1);
+                    double sq_err = calculate_in_error(inner, inner_squared_sums, in_points) + calculate_out_error(outer, outer_squared_sums, in_points, nx * ny);
+
                     if (sq_err < min_err)
                     {
                         min_err = sq_err;
@@ -387,44 +316,6 @@ Result segment(int ny, int nx, const float *data)
                             result.outer[c] = outer[c];
                         }
                     }
-
-                    // TODO delete
-                    printf("x0: %d, x1: %d, y0: %d, y1: %d\n", x0, x1, y0, y1);
-
-                    double outer2[C] = {0.0};
-                    double inner2[C] = {0.0};
-                    calculate_avg_in_color(x0, x1 - 1, y0, y1 - 1, nx, avg_from_zero, inner2);
-                    calculate_avg_out_color(x0, x1 - 1, y0, y1 - 1, nx, ny, inner2, total_avg, outer2);
-
-                    double outer_squared_sums[C] = {0.0};
-                    double inner_squared_sums[C] = {0.0};
-                    calculate_in_squared_sum(x0, x1 - 1, y0, y1 - 1, nx, sum_squared_from_zero, inner_squared_sums);
-                    calculate_out_squared_sum(nx, ny, inner_squared_sums, sum_squared_from_zero, outer_squared_sums);
-                    int in_points = calculate_in_points(x0, x1 - 1, y0, y1 - 1);
-                    double sq_err2 = calculate_in_error(inner2, inner_squared_sums, in_points) + calculate_out_error(outer2, outer_squared_sums, in_points, nx * ny);
-
-                    for (int c = 0; c < C; c++)
-                        printf("%d: %f-%f = %f-%f\n", c, inner[c], inner2[c], outer[c], outer2[c]);
-                    printf("sq_err: %f - %f\n", sq_err, sq_err2);
-                    for (int c = 0; c < C; c++)
-                    {
-                        if ((inner2[c] - inner[c]) * (inner2[c] - inner[c]) > epsilon || (outer2[c] - outer[c]) * (outer2[c] - outer[c]) > epsilon)
-                        {
-                            printf("ERROR in %d\n", c);
-                            Result result{0, 0, 0, 0, {0, 0, 0}, {0, 0, 0}};
-                            return result;
-                        }
-                    }
-                    if ((sq_err - sq_err2) * (sq_err - sq_err2) > epsilon)
-                    {
-                        {
-                            printf("ERROR in sq_err\n");
-                            Result result{0, 0, 0, 0, {0, 0, 0}, {0, 0, 0}};
-                            return result;
-                        }
-                    }
-
-                    printf("ok\n\n");
                 }
             }
         }
