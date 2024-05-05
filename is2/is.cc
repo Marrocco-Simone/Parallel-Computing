b0138a050a5c7b6939d5d560b3e6c7683bbc38f3
Marrocco-Simone <marrocco.simone00@gmail.com>
2024-05-05 10:50:29 +0300

    working functions for efficient average

--- is2/is.cc
+++ is2/is.cc
@@ -1,10 +1,14 @@
 // #include <limits>
-// #include <cstdio>
+// TODO delete
+#include <cstdio>
+#include <vector>
 using namespace std;
 
 // constexpr double infty = numeric_limits<double>::infinity();
 constexpr double infty = 1.7e308;
 constexpr int C = 3;
+// TODO delete
+constexpr double epsilon = 1e-10;
 
 struct Result
 {
@@ -16,9 +20,14 @@ struct Result
     float inner[C];
 };
 
+int id(int x, int y, int c, int nx)
+{
+    return c + C * x + C * nx * y;
+}
+
 double color(int x, int y, int c, int nx, const float *data)
 {
-    return data[c + C * x + C * nx * y];
+    return data[id(x, y, c, nx)];
 }
 
 double error(int x, int y, int c, int nx, const float *data, double *area)
@@ -81,6 +90,131 @@ void avg_colors(int x0, int x1, int y0, int y1, int nx, int ny, const float *dat
     }
 }
 
+/** O(n^2)*/
+void calculate_total_avg(int ny, int nx, const float *data, double *total_avg)
+{
+    int tot = ny * nx;
+    for (int x = 0; x < nx; x++)
+        for (int y = 0; y < ny; y++)
+            for (int c = 0; c < C; c++)
+                total_avg[c] += color(x, y, c, nx, data) / tot;
+}
+
+/** O(n^2) */
+void calculate_avg_from_zero(int ny, int nx, const float *data, vector<double> &avg_from_zero)
+{
+    for (int x = 0; x < nx; x++)
+        for (int y = 0; y < ny; y++)
+            for (int c = 0; c < C; c++)
+            {
+                printf("zero: %d %d %d\n", x, y, c);
+                if (y == 0)
+                {
+                    if (x == 0)
+                    {
+                        // * set first element
+                        avg_from_zero[id(x, y, c, nx)] = color(x, y, c, nx, data);
+                    }
+                    else
+                    {
+                        // * set first row
+                        double prev_left_block = avg_from_zero[id(x - 1, y, c, nx)] * ((y + 1) * x);
+                        avg_from_zero[id(x, y, c, nx)] = (prev_left_block + color(x, y, c, nx, data)) / ((x + 1) * (y + 1));
+                    }
+                }
+                else if (x == 0)
+                {
+                    // * set first column
+                    double prev_up_block = avg_from_zero[id(x, y - 1, c, nx)] * (y * (x + 1));
+                    avg_from_zero[id(x, y, c, nx)] = (prev_up_block + color(x, y, c, nx, data)) / ((x + 1) * (y + 1));
+                }
+                else
+                {
+                    double prev_left_block = avg_from_zero[id(x - 1, y, c, nx)] * ((y + 1) * x);
+                    double prev_up_block = avg_from_zero[id(x, y - 1, c, nx)] * (y * (x + 1));
+                    double prev_left_up_block = avg_from_zero[id(x - 1, y - 1, c, nx)] * (y * x);
+
+                    avg_from_zero[id(x, y, c, nx)] = (prev_left_block + prev_up_block - prev_left_up_block + color(x, y, c, nx, data)) / ((x + 1) * (y + 1));
+                }
+            }
+}
+
+/** O(1) */
+void calculate_avg_in_color(int x0, int x1, int y0, int y1, int nx, vector<double> const &avg_from_zero, double *in)
+{
+    int block_points = (y1 - y0 + 1) * (x1 - x0 + 1);
+    for (int c = 0; c < C; c++)
+    {
+        if (y0 == 0)
+        {
+            if (x0 == 0)
+            {
+                in[c] = avg_from_zero[id(x1, y1, c, nx)];
+            }
+            else
+            {
+                double point_block = avg_from_zero[id(x1, y1, c, nx)] * ((y1 + 1) * (x1 + 1));
+                double prev_left_block = avg_from_zero[id(x0 - 1, y1, c, nx)] * ((y1 + 1) * x0);
+                in[c] = (point_block - prev_left_block) / block_points;
+            }
+        }
+        else if (x0 == 0)
+        {
+            double point_block = avg_from_zero[id(x1, y1, c, nx)] * ((y1 + 1) * (x1 + 1));
+            double prev_up_block = avg_from_zero[id(x1, y0 - 1, c, nx)] * (y0 * (x1 + 1));
+            in[c] = (point_block - prev_up_block) / block_points;
+        }
+        else if (y1 == 0)
+        {
+            // if (x1 == 0)
+            // {
+            //     // * set first element
+            //     in[c] = avg_from_zero[id(x1, y1, c, nx)];
+            // }
+            // else
+            {
+                // * set first row
+                printf("ids: %d\t%d\n", id(x0 - 1, y1, c, nx), id(x1, y1, c, nx));
+                double x0y1_block = avg_from_zero[id(x0 - 1, y1, c, nx)] * x0;
+                double x1y1_block = avg_from_zero[id(x1, y1, c, nx)] * (x1 + 1);
+                in[c] = (x1y1_block - x0y1_block) / block_points;
+            }
+        }
+        else if (x1 == 0)
+        {
+            // * set first column
+            double x1y0_block = avg_from_zero[id(x1, y0 - 1, c, nx)] * y0;
+            double x1y1_block = avg_from_zero[id(x1, y1, c, nx)] * (y1 + 1);
+            in[c] = (x1y1_block - x1y0_block) / block_points;
+        }
+        else
+        {
+            double x0y0_block = avg_from_zero[id(x0 - 1, y0 - 1, c, nx)] * (y0 * x0);
+            double x1y0_block = avg_from_zero[id(x1, y0 - 1, c, nx)] * (y0 * (x1 + 1));
+            double x0y1_block = avg_from_zero[id(x0 - 1, y1, c, nx)] * ((y1 + 1) * x0);
+            double x1y1_block = avg_from_zero[id(x1, y1, c, nx)] * ((y1 + 1) * (x1 + 1));
+            in[c] = (x0y0_block + x1y1_block - x1y0_block - x0y1_block) / block_points;
+        }
+    }
+}
+
+/** O(1) */
+void calculate_avg_out_color(int x0, int x1, int y0, int y1, int nx, int ny, double *in, double *total_avg, double *out)
+{
+    int i_points = (y1 - y0 + 1) * (x1 - x0 + 1);
+    int full_points = (nx * ny);
+    if (i_points == full_points)
+    {
+        return;
+    }
+    for (int c = 0; c < C; c++)
+    {
+        double in_block = in[c] * i_points;
+        double full_block = total_avg[c] * full_points;
+        out[c] = (full_block - in_block) / (full_points - i_points);
+    }
+}
+
 /*
 This is the function you need to implement. Quick reference:
 - x coordinates: 0 <= x < nx
@@ -92,6 +226,16 @@ Result segment(int ny, int nx, const float *data)
 {
     Result result{0, 0, 0, 0, {0, 0, 0}, {0, 0, 0}};
     double min_err = infty;
+    double total_avg[C] = {0.0};
+    calculate_total_avg(ny, nx, data, total_avg);
+    vector<double> avg_from_zero(C * nx * ny, 0.0);
+    calculate_avg_from_zero(ny, nx, data, avg_from_zero);
+
+    // TODO delete
+    for (int x = 0; x < nx; x++)
+        for (int y = 0; y < ny; y++)
+            printf("%f %f %f\n", avg_from_zero[id(x, y, 0, nx)], avg_from_zero[id(x, y, 1, nx)], avg_from_zero[id(x, y, 2, nx)]);
+    printf("\n");
 
     for (int xdim = 1; xdim <= nx; xdim++)
     {
@@ -103,8 +247,8 @@ Result segment(int ny, int nx, const float *data)
                 {
                     int x1 = x0 + xdim;
                     int y1 = y0 + ydim;
-                    double outer[C];
-                    double inner[C];
+                    double outer[C] = {0.0};
+                    double inner[C] = {0.0};
 
                     // printf("x0: %d, x1: %d, y0: %d, y1: %d\n", x0, x1, y0, y1);
                     avg_colors(x0, x1, y0, y1, nx, ny, data, outer, inner);
@@ -126,6 +270,27 @@ Result segment(int ny, int nx, const float *data)
                             result.outer[c] = outer[c];
                         }
                     }
+
+                    // TODO delete
+                    printf("x0: %d, x1: %d, y0: %d, y1: %d\n", x0, x1, y0, y1);
+
+                    double outer2[C] = {0.0};
+                    double inner2[C] = {0.0};
+                    calculate_avg_in_color(x0, x1 - 1, y0, y1 - 1, nx, avg_from_zero, inner2);
+                    calculate_avg_out_color(x0, x1 - 1, y0, y1 - 1, nx, ny, inner2, total_avg, outer2);
+
+                    for (int c = 0; c < C; c++)
+                        printf("%d: %f-%f = %f-%f\n", c, inner[c], inner2[c], outer[c], outer2[c]);
+                    for (int c = 0; c < C; c++)
+                    {
+                        if (inner2[c] - inner[c] > epsilon || outer2[c] - outer[c] > epsilon)
+                        {
+                            printf("ERROR in %d\n", c);
+                            Result result{0, 0, 0, 0, {0, 0, 0}, {0, 0, 0}};
+                            return result;
+                        }
+                    }
+                    printf("ok\n\n");
                 }
             }
         }
