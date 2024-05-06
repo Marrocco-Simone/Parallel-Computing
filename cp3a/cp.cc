46b0f1a085163724718b450e8ce88418de007cc9
Marrocco-Simone <marrocco.simone00@gmail.com>
2024-05-06 19:10:17 +0300

    first test with full parallelism cp3a

--- cp3a/cp.cc
+++ cp3a/cp.cc
@@ -1,3 +1,11 @@
+#include <vector>
+#include <cmath>
+#define ll vector<double>
+typedef double double4_t __attribute__((vector_size(4 * sizeof(double))));
+#define ll4_t vector<double4_t>
+
+using namespace std;
+
 /*
 This is the function you need to implement. Quick reference:
 - input rows: 0 <= y < ny
@@ -6,5 +14,71 @@ This is the function you need to implement. Quick reference:
 - correlation between rows i and row j has to be stored in result[i + j*ny]
 - only parts with 0 <= j <= i < ny need to be filled
 */
-void correlate(int ny, int nx, const float *data, float *result) {
+void correlate(int ny, int nx, const float *data, float *result)
+{
+  int next_mul_of_4 = nx + (4 - nx % 4) % 4;
+  int nnx = next_mul_of_4 / 4;
+  ll4_t normalized(ny * nnx);
+#pragma omp parallel for
+  for (int j = 0; j < ny; j++)
+  {
+    double mean = 0.0;
+    double magnitude = 0.0;
+
+    for (int i = 0; i < nx; i++)
+      mean += data[i + j * nx] / nx;
+
+    for (int i = 0; i < nx; i++)
+    {
+      int ii = i % 4;
+      double v = (data[i + j * nx] - mean);
+      normalized[i / 4 + j * nnx][ii] = v;
+      magnitude += v * v;
+    }
+    magnitude = sqrtl(magnitude);
+
+    for (int i = 0; i < nnx; i++)
+    {
+      normalized[i + j * nnx] /= magnitude;
+    }
+  }
+
+#pragma omp parallel for
+  for (int j = 0; j < ny; j++)
+  {
+    for (int i = j; i < ny; i++)
+    {
+      double4_t sum1 = {0.0};
+      double4_t sum2 = {0.0};
+      double4_t sum3 = {0.0};
+      double4_t sum4 = {0.0};
+      int k = 0;
+      while (k < nnx - 4)
+      {
+        sum1 += normalized[k + 0 + i * nnx] * normalized[k + 0 + j * nnx];
+        sum2 += normalized[k + 1 + i * nnx] * normalized[k + 1 + j * nnx];
+        sum3 += normalized[k + 2 + i * nnx] * normalized[k + 2 + j * nnx];
+        sum4 += normalized[k + 3 + i * nnx] * normalized[k + 3 + j * nnx];
+        k += 4;
+      }
+      while (k < nnx)
+      {
+        sum1 += normalized[k + 0 + i * nnx] * normalized[k + 0 + j * nnx];
+        k++;
+      }
+
+      double cumsum1 = 0.0;
+      double cumsum2 = 0.0;
+      double cumsum3 = 0.0;
+      double cumsum4 = 0.0;
+      for (int ii = 0; ii < 4; ii++)
+      {
+        cumsum1 += sum1[ii];
+        cumsum2 += sum2[ii];
+        cumsum3 += sum3[ii];
+        cumsum4 += sum4[ii];
+      }
+      result[i + j * ny] = cumsum1 + cumsum2 + cumsum3 + cumsum4;
+    }
+  }
 }
