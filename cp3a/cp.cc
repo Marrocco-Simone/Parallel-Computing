10dac3cfd4dd70f07dd4718f70d6302f6c491e34
Marrocco-Simone <marrocco.simone00@gmail.com>
2024-05-07 17:48:54 +0300

    easier cumulative sum

--- cp3a/cp.cc
+++ cp3a/cp.cc
@@ -48,45 +48,26 @@ void correlate(int ny, int nx, const float *data, float *result)
   {
     for (int i = j; i < ny; i++)
     {
-      // ll4_t sum(4);
-      double4_t sum0 = {0.0};
-      double4_t sum1 = {0.0};
-      double4_t sum2 = {0.0};
-      double4_t sum3 = {0.0};
+      ll4_t sum(4);
       int k = 0;
       while (k < nnx - 4)
       {
-        // for (int kk = 0; kk < 4; kk++)
-        //   sum[kk] = normalized[k + kk + i * nnx] * normalized[k + kk + j * nnx];
-        sum0 += normalized[k + 0 + i * nnx] * normalized[k + 0 + j * nnx];
-        sum1 += normalized[k + 1 + i * nnx] * normalized[k + 1 + j * nnx];
-        sum2 += normalized[k + 2 + i * nnx] * normalized[k + 2 + j * nnx];
-        sum3 += normalized[k + 3 + i * nnx] * normalized[k + 3 + j * nnx];
+        for (int kk = 0; kk < 4; kk++)
+          sum[kk] += normalized[k + kk + i * nnx] * normalized[k + kk + j * nnx];
         k += 4;
       }
       while (k < nnx)
       {
-        sum0 += normalized[k + 0 + i * nnx] * normalized[k + 0 + j * nnx];
-        // sum[0] += normalized[k + i * nnx] * normalized[k + j * nnx];
+        sum[0] += normalized[k + i * nnx] * normalized[k + j * nnx];
         k++;
       }
 
-      double cumsum0 = 0.0;
-      double cumsum1 = 0.0;
-      double cumsum2 = 0.0;
-      double cumsum3 = 0.0;
+      double4_t cumsum = {0.0};
       for (int ii = 0; ii < 4; ii++)
       {
-        // cumsum0 += sum[0][ii];
-        // cumsum1 += sum[1][ii];
-        // cumsum2 += sum[2][ii];
-        // cumsum3 += sum[3][ii];
-        cumsum0 += sum0[ii];
-        cumsum1 += sum1[ii];
-        cumsum2 += sum2[ii];
-        cumsum3 += sum3[ii];
+        cumsum += sum[ii];
       }
-      result[i + j * ny] = cumsum0 + cumsum1 + cumsum2 + cumsum3;
+      result[i + j * ny] = cumsum[0] + cumsum[1] + cumsum[2] + cumsum[3];
     }
   }
 }
