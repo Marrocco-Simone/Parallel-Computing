e248e2c2a15f1fc9586df3b8bc7bf48c6a316292
Marrocco-Simone <marrocco.simone00@gmail.com>
2024-05-10 01:48:52 +0300

    simple copuy of cp3a

--- cp3b/cp.cc
+++ cp3b/cp.cc
@@ -1,3 +1,67 @@
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <immintrin.h>
+#include <tuple>
+#define ll vector<float>
+/** parameter for vectoring operations - dont touch, dependeant on float implementation */
+#define N 8
+/** parameter for multiple calculations between rows */
+#define S 8
+#define infinity 2147483647
+typedef float float8_t __attribute__((vector_size(N * sizeof(float))));
+#define ll4_t vector<float8_t>
+
+using namespace std;
+
+void normalize_rows(int ny, int nx, int nnx, const float *data, ll4_t &normalized)
+{
+#pragma omp parallel for
+  for (int j = 0; j < ny; j++)
+  {
+    float mean = 0.0;
+    float magnitude = 0.0;
+
+    for (int i = 0; i < nx; i++)
+      mean += data[i + j * nx] / nx;
+
+    for (int i = 0; i < nx; i++)
+    {
+      int ii = i % N;
+      float v = (data[i + j * nx] - mean);
+      normalized[i / N + j * nnx][ii] = v;
+      magnitude += v * v;
+    }
+    magnitude = sqrtl(magnitude);
+
+    for (int i = 0; i < nnx; i++)
+    {
+      normalized[i + j * nnx] /= magnitude;
+    }
+  }
+}
+
+void set_result_to_zero(int ny, float *result)
+{
+#pragma omp parallel for
+  for (int j = 0; j < ny; j++)
+    for (int i = j; i < ny; i++)
+      result[i + j * ny] = 0.0;
+}
+
+void get_rows_order(int ny, vector<tuple<int, int, int>> &rows)
+{
+  int count = 0;
+  for (int j = 0; j < ny; j += S)
+    for (int i = j; i < ny; i += S)
+    {
+      int ji = _pdep_u32(j, 0x55555555) | _pdep_u32(i, 0xAAAAAAAA);
+      rows[count] = make_tuple(ji, j, i);
+      count++;
+    }
+  sort(rows.begin(), rows.end());
+}
+
 /*
 This is the function you need to implement. Quick reference:
 - input rows: 0 <= y < ny
@@ -6,5 +70,39 @@ This is the function you need to implement. Quick reference:
 - correlation between rows i and row j has to be stored in result[i + j*ny]
 - only parts with 0 <= j <= i < ny need to be filled
 */
-void correlate(int ny, int nx, const float *data, float *result) {
+void correlate(int ny, int nx, const float *data, float *result)
+{
+  set_result_to_zero(ny, result);
+
+  int next_mul_of_N = nx + (N - nx % N) % N;
+  int nnx = next_mul_of_N / N;
+  ll4_t normalized(ny * nnx);
+  normalize_rows(ny, nx, nnx, data, normalized);
+
+  int p = (ny - 1) / S;
+  int dim = 0.5 * (p * p) + 1.5 * (p) + 1;
+  vector<tuple<int, int, int>> rows(dim);
+  get_rows_order(ny, rows);
+
+#pragma omp parallel for
+  for (auto [_, j, i] : rows)
+  {
+    int s1_limit = min(S, ny - j);
+    int s2_limit = min(S, ny - i);
+
+    ll4_t sum(S * S);
+    for (int k = 0; k < nnx; k++)
+      for (int s1 = 0; s1 < s1_limit; s1++)
+        for (int s2 = 0; s2 < s2_limit; s2++)
+          sum[s2 + s1 * S] += normalized[k + (i + s2) * nnx] * normalized[k + (j + s1) * nnx];
+
+    ll cumsum(S * S, 0.0);
+    for (int s = 0; s < S * S; s++)
+      for (int h = 0; h < N; h++)
+        cumsum[s] += sum[s][h];
+
+    for (int s1 = 0; s1 < s1_limit; s1++)
+      for (int s2 = 0; s2 < s2_limit; s2++)
+        result[i + s2 + (j + s1) * ny] = cumsum[s2 + s1 * S];
+  }
 }
