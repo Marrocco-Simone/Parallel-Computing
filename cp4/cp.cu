e38601b55b4ddf4cfb5302a44d43a8c2a35a7f58
Marrocco-Simone <marrocco.simone00@gmail.com>
2024-05-17 20:13:14 +0300

    normalized in gpu

--- cp4/cp.cu
+++ cp4/cp.cu
@@ -18,27 +18,28 @@ static inline void check(cudaError_t err, const char *context)
 
 #define CHECK(x) check(x, #x)
 
-void normalize_rows(int ny, int nx, const float *data, float *normalized)
+__global__ void normalize_rows(int ny, int nx, float *data, float *normalized)
 {
-  for (int j = 0; j < ny; j++)
+  int j = blockIdx.x * STEPS + threadIdx.x;
+  if (j >= ny)
+    return;
+
+  float mean = 0.0;
+  float magnitude = 0.0;
+
+  for (int i = 0; i < nx; i++)
+    mean += data[i + j * nx] / nx;
+
+  for (int i = 0; i < nx; i++)
   {
-    float mean = 0.0;
-    float magnitude = 0.0;
+    normalized[i + j * nx] = (data[i + j * nx] - mean);
+    magnitude += normalized[i + j * nx] * normalized[i + j * nx];
+  }
+  magnitude = sqrtf(magnitude);
 
-    for (int i = 0; i < nx; i++)
-      mean += data[i + j * nx] / nx;
-
-    for (int i = 0; i < nx; i++)
-    {
-      normalized[i + j * nx] = (data[i + j * nx] - mean);
-      magnitude += normalized[i + j * nx] * normalized[i + j * nx];
-    }
-    magnitude = sqrtf(magnitude);
-
-    for (int i = 0; i < nx; i++)
-    {
-      normalized[i + j * nx] /= magnitude;
-    }
+  for (int i = 0; i < nx; i++)
+  {
+    normalized[i + j * nx] /= magnitude;
   }
 }
 
@@ -46,16 +47,15 @@ __global__ void calculate_result(int nx, int ny, float *result, float *normalize
 {
   int is = blockIdx.x * STEPS;
   int js = threadIdx.x * STEPS;
+  int ie = min(is + STEPS, ny);
+  int je = min(js + STEPS, ny);
 
   if (is >= ny || js >= ny)
     return;
 
-  for (int j = js; j < js + STEPS; j++)
-    for (int i = is; i < is + STEPS; i++)
+  for (int j = js; j < je; j++)
+    for (int i = is; i < ie; i++)
     {
-      if (i >= ny || j >= ny)
-        continue;
-
       float sum = 0.0;
       for (int k = 0; k < nx; ++k)
         sum += normalized[k + i * nx] * normalized[k + j * nx];
@@ -76,21 +76,23 @@ void correlate(int ny, int nx, const float *data, float *result)
 {
   int nnx = nx + STEPS - nx % STEPS;
   int nny = ny + STEPS - ny % STEPS;
-  // float *normalized = (float *)malloc(nnx * nny * sizeof(float));
-  float *normalized = (float *)calloc(nnx * nny, sizeof(float));
-  normalize_rows(ny, nx, data, normalized);
 
-  // Allocate memory & copy data to GPU
+  float *dataGPU = NULL;
+  CHECK(cudaMalloc((void **)&dataGPU, nx * ny * sizeof(float)));
+  CHECK(cudaMemcpy(dataGPU, data, nx * ny * sizeof(float), cudaMemcpyHostToDevice));
+
   float *normalizedGPU = NULL;
   CHECK(cudaMalloc((void **)&normalizedGPU, nnx * nny * sizeof(float)));
+  normalize_rows<<<nny / STEPS, STEPS>>>(ny, nx, dataGPU, normalizedGPU);
+
   float *resultGPU = NULL;
   CHECK(cudaMalloc((void **)&resultGPU, ny * ny * sizeof(float)));
-  CHECK(cudaMemcpy(normalizedGPU, normalized, nnx * nny * sizeof(float), cudaMemcpyHostToDevice));
 
   calculate_result<<<nny / STEPS, nny / STEPS>>>(nx, ny, resultGPU, normalizedGPU);
   CHECK(cudaGetLastError());
 
   CHECK(cudaMemcpy(result, resultGPU, ny * ny * sizeof(float), cudaMemcpyDeviceToHost));
+  CHECK(cudaFree(dataGPU));
   CHECK(cudaFree(normalizedGPU));
   CHECK(cudaFree(resultGPU));
 }
