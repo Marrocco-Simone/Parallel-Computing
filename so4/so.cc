0ad9dc7c1a79b9268222282c93085c302983ca92
Marrocco-Simone <marrocco.simone00@gmail.com>
2024-05-19 01:10:50 +0300

    removed unused stuff

--- so4/so.cc
+++ so4/so.cc
@@ -22,37 +22,6 @@ void merge_sort(data_t *data, int start, int end, int min_sort)
     std::inplace_merge(data + start, data + mid, data + end);
 }
 
-void bottom_up(int n, data_t *data, int min_sort)
-{
-#pragma omp parallel for
-    for (int i = 0; i < n; i += min_sort)
-    {
-        int e = min(i + min_sort, n);
-        std::sort(data + i, data + e);
-    }
-    vector<data_t> tmp;
-    tmp.reserve(n);
-    for (int j = 2; j < n; j += j)
-#pragma omp parallel for
-        for (int i = 0; i < n; i += min_sort * j)
-        {
-            int m = i + min_sort * (j / 2);
-            int e = min(i + min_sort * j, n);
-            if (m >= n)
-                continue;
-            std::merge(data + i, data + m, data + m, data + e, tmp.begin() + i);
-            for (int k = i; k < e; k++)
-                data[k] = tmp[k];
-        }
-}
-
-void top_down(int n, data_t *data, int min_sort)
-{
-#pragma omp parallel
-#pragma omp single
-    merge_sort(data, 0, n, min_sort);
-}
-
 void psort(int n, data_t *data)
 {
     // FIXME: Implement a more efficient parallel sorting algorithm for the CPU,
@@ -62,6 +31,7 @@ void psort(int n, data_t *data)
     if (min_sort < 2)
         min_sort = 2;
 
-    // bottom_down(n, data, min_sort);
-    top_down(n, data, min_sort);
+#pragma omp parallel
+#pragma omp single
+    merge_sort(data, 0, n, min_sort);
 }
