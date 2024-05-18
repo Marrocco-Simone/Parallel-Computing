e6dd03969deac370845e3e47b5c9052450fa5017
Marrocco-Simone <marrocco.simone00@gmail.com>
2024-05-18 23:41:11 +0300

    dynamic min sort

--- so4/so.cc
+++ so4/so.cc
@@ -6,18 +6,18 @@ using namespace std;
 
 typedef unsigned long long data_t;
 
-void merge_sort(data_t *data, int start, int end)
+void merge_sort(data_t *data, int start, int end, int min_sort)
 {
-    if (end - start < MIN_SORT)
+    if (end - start < min_sort)
     {
         std::sort(data + start, data + end);
         return;
     }
     int mid = (start + end) / 2;
 #pragma omp task
-    merge_sort(data, start, mid);
+    merge_sort(data, start, mid, min_sort);
 #pragma omp task
-    merge_sort(data, mid, end);
+    merge_sort(data, mid, end, min_sort);
 #pragma omp taskwait
     {
         vector<data_t> tmp;
@@ -34,7 +34,10 @@ void psort(int n, data_t *data)
     // FIXME: Implement a more efficient parallel sorting algorithm for the CPU,
     // using the basic idea of merge sort.
     // std::sort(data, data + n);
+    int min_sort = n / MIN_SORT;
+    if (min_sort < 2)
+        min_sort = 2;
 #pragma omp parallel
 #pragma omp single
-    merge_sort(data, 0, n);
+    merge_sort(data, 0, n, min_sort);
 }
