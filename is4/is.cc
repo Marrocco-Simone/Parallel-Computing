c6127cdd20c7c5a3842664e8740c7dfd6e9393da
Marrocco-Simone <marrocco.simone00@gmail.com>
2024-05-19 19:06:08 +0300

    move calculations of best points afterward

--- is4/is.cc
+++ is4/is.cc
@@ -1,4 +1,6 @@
 #include <vector>
+#include <chrono>
+#include <cstdio>
 #define T 4
 typedef double double4_t __attribute__((vector_size(T * sizeof(double))));
 using namespace std;
@@ -112,20 +114,27 @@ This is the function you need to implement. Quick reference:
 */
 Result segment(int ny, int nx, const float *data)
 {
+    using std::chrono::duration;
+    using std::chrono::duration_cast;
+    using std::chrono::high_resolution_clock;
+    using std::chrono::milliseconds;
+
+    auto t1 = high_resolution_clock::now();
+
     Result result{0, 0, 0, 0, {0, 0, 0}, {0, 0, 0}};
     double4_t total_sum = {0.0};
     calculate_total_avg(ny, nx, data, total_sum);
     vector<double4_t> sum_from_zero(nx * ny);
     calculate_sum_from_zero(ny, nx, data, sum_from_zero);
 
-    vector<int> best_solutions_coord(nx * ny * 2);
-    vector<double> best_solutions(nx * ny);
+    vector<double> best_solutions(nx * ny, 0.0);
     int full_points = nx * ny;
+
+    auto t2 = high_resolution_clock::now();
 
 #pragma omp parallel for schedule(dynamic, 2)
     for (int x0y0 = 0; x0y0 < ny * nx; x0y0++)
     {
-        double max_err = 0.0;
         int i = x0y0;
         int y0 = x0y0 / nx;
         int x0 = x0y0 % nx;
@@ -143,16 +152,12 @@ Result segment(int ny, int nx, const float *data)
 
                 double inv_sq_err = (inner[0] * inner[0] + inner[1] * inner[1] + inner[2] * inner[2]) / in_points + (outer[0] * outer[0] + outer[1] * outer[1] + outer[2] * outer[2]) / out_points;
 
-                if (inv_sq_err > max_err)
-                {
-                    max_err = inv_sq_err;
-                    best_solutions[i] = inv_sq_err;
-                    best_solutions_coord[i * 2 + 0] = x1;
-                    best_solutions_coord[i * 2 + 1] = y1;
-                }
+                best_solutions[i] = max(best_solutions[i], inv_sq_err);
             }
         }
     }
+
+    auto t3 = high_resolution_clock::now();
 
     int min_i = 0;
     for (int i = 0; i < nx * ny; i++)
@@ -161,9 +166,38 @@ Result segment(int ny, int nx, const float *data)
 
     int y0 = min_i / nx;
     int x0 = min_i % nx;
-    int x1 = best_solutions_coord[min_i * 2 + 0];
-    int y1 = best_solutions_coord[min_i * 2 + 1];
-    set_result(x0, x1, y0, y1, nx, ny, sum_from_zero, total_sum, result);
+
+    int y1f = y0;
+    int x1f = x0;
+    double max_err = 0.0;
+    for (int y1 = y0; y1 < ny; y1++)
+    {
+        for (int x1 = x0; x1 < nx; x1++)
+        {
+            int in_points = calculate_in_points(x0, x1, y0, y1);
+            int out_points = full_points - in_points;
+
+            double4_t inner;
+            calculate_avg_in_color(x0, x1, y0, y1, nx, sum_from_zero, inner);
+            double4_t outer = total_sum - inner;
+
+            double inv_sq_err = (inner[0] * inner[0] + inner[1] * inner[1] + inner[2] * inner[2]) / in_points + (outer[0] * outer[0] + outer[1] * outer[1] + outer[2] * outer[2]) / out_points;
+
+            if (inv_sq_err > max_err)
+            {
+                max_err = inv_sq_err;
+                y1f = y1;
+                x1f = x1;
+            }
+        }
+    }
+    set_result(x0, x1f, y0, y1f, nx, ny, sum_from_zero, total_sum, result);
+
+    auto t4 = high_resolution_clock::now();
+
+    printf("Initialization: %ld ms\n", duration_cast<milliseconds>(t2 - t1).count());
+    printf("Main loop: %ld ms\n", duration_cast<milliseconds>(t3 - t2).count());
+    printf("Finalization: %ld ms\n", duration_cast<milliseconds>(t4 - t3).count());
 
     return result;
 }
