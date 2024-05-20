17037cfe046d236e398996e3175644860d1b44cb
Marrocco-Simone <marrocco.simone00@gmail.com>
2024-05-20 17:35:45 +0300

    nvm

--- cp5/cp.cu
+++ cp5/cp.cu
@@ -50,8 +50,15 @@ __global__ void calculate_result(int nx, int ny, float *result, float *normalize
   int je = min(js + STEPS, ny);
   int ie = min(is + STEPS, ny);
 
-  if (ie < js)
+  if (is >= ny || js >= ny)
     return;
+  if (ie < js)
+  {
+    for (int j = js; j < je; j++)
+      for (int i = is; i < ie; i++)
+        result[i + j * ny] = 0.0;
+    return;
+  }
 
   for (int j = js; j < je; j++)
     for (int i = is; i < ie; i++)
